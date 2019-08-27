a86c453529a94c921671696120b4a4c59bd1cbb6
Joachen D Busch <joachendbusch@gmail.com>
2019-08-26 22:19:26 -0700

    updates

--- code401challenges/stacksandqueues/src/main/java/stacksandqueues/PseudoQueue.java
+++ code401challenges/stacksandqueues/src/main/java/stacksandqueues/PseudoQueue.java
@@ -10,21 +10,25 @@ public PseudoQueue () {
     this.stack1 = new Stack<>();
     this.stack2 = new Stack<>();
 }
-
+//push value to stack 1
 public void enqueue (T value) {
     this.stack1.push(value);
 }
 
 public T dequeue () {
+    //if both stacks are empty return null
     if (this.stack1 == null && this.stack2 == null) {
         return null;
     } else {
+        //if stack 2 is empty
         if (this.stack2 == null) {
+            //while stack 1 is not
             while (this.stack1 != null) {
+                //push stack 1 to stack 2 and pop from stack
                 this.stack2.push(this.stack1.pop());
             }
         }
-
+        //return the value
         return this.stack2.pop();
     }
 }
