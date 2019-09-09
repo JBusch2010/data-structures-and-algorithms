89e2c3b88ada80c9395c2c6eeb42ee674cf8d2a2
Joachen D Busch <joachendbusch@gmail.com>
2019-09-09 13:33:26 -0700

    insertionsort func

---
+++ code401challenges/insertionsort/src/test/java/insertionsort/InsertionSortTest.java
@@ -0,0 +1,14 @@
+package insertionsort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InsertionSortTest {
+
+    @Test
+    public void insertionSort () {
+        int arr[] = { 12, 11, 13, 5, 6 };
+
+    }
+}
